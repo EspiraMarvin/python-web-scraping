2f80c90a403b89e1ab8cda1b8bb26c6271071c4d
EspiraMarvin <espiramarvin@gmail.com>
2021-07-27 01:07:01 +0300

    selenium

---
+++ selenium_scrape.py
@@ -0,0 +1,7 @@
+import os
+import selenium
+from selenium import webdriver
+import time
+import requests
+from webdriver_manager.chrome import chromeDriverManager
+from selenium.common.exceptions import ElementClickInterceptedException
