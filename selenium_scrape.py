3952cf5d69fcdedbf7757750d13fc5c50ae051a0
EspiraMarvin <espiramarvin@gmail.com>
2021-07-27 01:33:38 +0300

    supporting packages

--- selenium_scrape.py
+++ selenium_scrape.py
@@ -1,7 +1,13 @@
-import os
-import selenium
+# import selenium
+# from selenium import webdriver
+# from selenium.common.exceptions import ElementClickInterceptedException
+
 from selenium import webdriver
-import time
-import requests
-from webdriver_manager.chrome import chromeDriverManager
-from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait, Select
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+url = 'https://books.toscrape.com/'
+driver = webdriver.Chrome('/home/marvin/chromedriver')
+driver.get(url)
